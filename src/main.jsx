7ad52567e0601c183b41af8d1b44af57a87f335f
eonickth-boop <eonickth@gmail.com>
2026-07-18 02:10:00 -0300

    feat: registrar rota do RPG textual

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
-﻿import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import RpgCenas from './pages/RpgCenas.jsx'
 import SidebarGlobal from './components/layout/SidebarGlobal.jsx'
+
+const paginaRpg = window.location.pathname.startsWith('/rpg')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>
-    <SidebarGlobal />
-    <App />
-  </>
+    {paginaRpg ? (
+      <RpgCenas />
+    ) : (
+      <>
+        <SidebarGlobal />
+        <App />
+      </>
+    )}
   </StrictMode>,
 )
-
-
